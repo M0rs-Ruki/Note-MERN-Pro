5c8d07673d2576c8f65214b4fe4648593b4accae
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-30 22:45:16 +0530

    Connecting to a database MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,25 @@ import { log } from 'console';
 import path from 'path';
 import connectDB from './src/db/connect.db.js';
 
-dotenv.config(path="./.env");
+dotenv.config({path:"./.env"});
 const app = express();
 const port = 3000;
 
 
-// Database Connexion
+// Connect to the database and then start the server
 connectDB()
 .then(() => {
-    app.listen(port, () => {
-        log(`⚡️[server]: Server is running at http://localhost:${process.env.PORT}`);
-    })
     app.on("error", (err) => {
-        log(`⚡️[server]: Server error: ${err}`)
+        log(`SERVER ERROR: ${err}`);
+        throw err;
+    });
+
+    app.listen(port, () => {
+        log(`⚡️ Server is running at http://localhost:${port}`);
     });
 })
 .catch((err) => {
-    log(`⚡️[MONGODB]:error: ${err}`)
+    log(`❌ MONGODB CONNECTION FAILED: ${err}`);
 });
 
 // Middleware
--- backend/src/db/connect.db.js
+++ backend/src/db/connect.db.js
@@ -1,16 +1,23 @@
-
-import mongooes from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { log } from 'console';
-dotenv.config(path="./.env");
+
+dotenv.config({ path: "./.env" });
 
 const connectDB = async () => {
+    if (!process.env.MongoDB_URL || !process.env.MONGO_DB_NAME) {
+        log('Error: MongoDB_URL or MONGO_DB_NAME not found in .env file.');
+        process.exit(1);
+    }
+    
     try {
-        const connectionInstance = await mongooes.connect
-        (`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`);
-        log(`MongoDB connected: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(process.env.MongoDB_URL, {
+            dbName: process.env.MONGO_DB_NAME,
+        });
+
+        log(`✅ MongoDB connected: ${connectionInstance.connection.host}`);
     } catch (error) {
-        log(`MongoDB connection error: ${error.message}`);
+        log(`❌ MongoDB connection error: ${error.message}`);
         process.exit(1);
     }
 }
